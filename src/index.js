6ced0a74361a819473df38a2feedf66a0810993e
unknown <anthonycardoze@gmail.com>
2020-11-14 01:06:06 -0500

    agregando hastrouter para tratar de resolver el problema en produccion

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { HashRouter } from 'react-router-dom';
+import './index.scss';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root"),
+	<HashRouter hashType={'noslash'}>
+		<App />
+	</HashRouter>,
+	document.getElementById('root'),
 );
 
 serviceWorker.unregister();
