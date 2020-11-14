ffe1c8abc2d05d19d2a546e57ed07c3b8354623b
unknown <anthonycardoze@gmail.com>
2020-11-14 13:11:53 -0500

    agregando fix a un bug

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,94 +1,94 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import * as url from "./urls";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import * as url from './urls';
+import { useHistory } from 'react-router-dom';
 
 export const requestHeader = (method, body = {}, token) => {
-  return {
-    method,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      Authorization: token ? `Bearer ${token}` : "",
-    },
-    mode: "cors",
-    cache: "default",
-    body: body,
-  };
+	return {
+		method,
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			Authorization: token ? `Bearer ${token}` : '',
+		},
+		mode: 'cors',
+		cache: 'default',
+		body: body,
+	};
 };
 
 requestHeader.PropTypes = {
-  method: PropTypes.string,
-  token: PropTypes.string,
-  body: PropTypes.object,
+	method: PropTypes.string,
+	token: PropTypes.string,
+	body: PropTypes.object,
 };
 
 export const fetchData = async (urls, header) => {
-  try {
-    const query = await fetch(urls, header);
-    const parsed = await query.json();
-    if (parsed === "No esta autorizado") {
-      const body = JSON.stringify({ token: localStorage.refresh_token });
-      const TokenRenewServiceUrl = url.refreshTokenUrl();
-      const headerRT = requestHeader("POST", body, "");
-      const loggedInfo = await fetchData(TokenRenewServiceUrl, headerRT);
-
-      if (loggedInfo.accessToken) {
-        localStorage.removeItem("token");
-        localStorage.setItem("token", loggedInfo.accessToken);
-
-        header.headers.Authorization = `Bearer ${loggedInfo.accessToken}`;
-
-        const requery = await fetch(urls, header);
-        const reparsed = await requery.json();
-        return reparsed;
-      } else {
-        return "No esta autorizado";
-      }
-    }
-    return parsed;
-  } catch (error) {
-    return "conexion error";
-  }
+	try {
+		const query = await fetch(urls, header);
+		const parsed = await query.json();
+		if (parsed === 'No esta autorizado') {
+			const body = JSON.stringify({
+				token: localStorage.refresh_token ? localStorage.refresh_token : '',
+			});
+			const TokenRenewServiceUrl = url.refreshTokenUrl();
+			const headerRT = requestHeader('POST', body, '');
+			const loggedInfo = await fetchData(TokenRenewServiceUrl, headerRT);
+			if (loggedInfo.accessToken) {
+				localStorage.setItem('token', loggedInfo.accessToken);
+				header.headers.Authorization = `Bearer ${loggedInfo.accessToken}`;
+				const requery = await fetch(urls, header);
+				const reparsed = await requery.json();
+				return reparsed;
+			} else {
+				return 'No esta autorizado';
+			}
+		} else {
+			return parsed;
+		}
+	} catch (error) {
+		console.log('error: ', error);
+		return 'conexion error';
+	}
 };
 
 export const SubcriberRefreshToken = ({ children }) => {
-  const history = useHistory();
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      if (localStorage.refresh_token === null || localStorage.token === null) {
-        localStorage.removeItem("token");
-        localStorage.removeItem("refresh_token");
-        history.push("/login");
-      }
-    }, 300000);
+	const history = useHistory();
+	useEffect(() => {
+		const interval = setInterval(async () => {
+			if (localStorage.refresh_token === null || localStorage.token === null) {
+				localStorage.removeItem('token');
+				localStorage.removeItem('refresh_token');
+				history.push('/login');
+			}
+		}, 10000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  });
-  return <>{children}</>;
+		return () => {
+			clearInterval(interval);
+		};
+	});
+	return <>{children}</>;
 };
 
 export function useStickyState(defaultValue, key) {
-  const [value, setValue] = useState(() => {
-    const stickyValue = localStorage.getItem(key);
-    return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
-  });
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
-  return [value, setValue];
+	const [value, setValue] = useState(() => {
+		const stickyValue = localStorage.getItem(key);
+		return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
+	});
+	useEffect(() => {
+		localStorage.setItem(key, JSON.stringify(value));
+	}, [key, value]);
+	return [value, setValue];
 }
 
 export const UnauthorizedRedirect = (data, history) => {
-  if (data === "conexion error") {
-    localStorage.removeItem("token");
-    localStorage.removeItem("refresh_token");
-    history.push("/login");
-  }
+	if (data === 'conexion error' || data === 'No esta autorizado') {
+		localStorage.removeItem('token');
+		localStorage.removeItem('refresh_token');
+		history.push('/login');
+	}
 };
 
 export const UserRedirect = (user, history) => {
-  if (user && user.rol !== "Administrador") return history.push("/profile");
+	if (user && user.rol !== 'Administrador') return history.push('/profile');
 };
--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,192 +1,192 @@
-import React, { useState, useEffect, useContext, memo } from "react";
-import { Box, Container, Switch } from "@material-ui/core";
-import Toolbar from "../../components/ToolBar/Toolbar";
-import MainLayout from "../../components/MainLayOut/mainLayout.component";
-import DataTable from "../../components/DataTable/databable";
-import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
-import * as url from "../../helpers/urls";
-import * as fetch from "../../helpers/fetch";
-import { useHistory } from "react-router-dom";
-import { Link } from "react-router-dom";
-import moment from "moment";
-import { UserContext } from "../../Context/userContext";
-import BackdropSpinner from "../../components/BackDrop/backDrop";
+import React, { useState, useEffect, useContext, memo } from 'react';
+import { Box, Container, Switch } from '@material-ui/core';
+import Toolbar from '../../components/ToolBar/Toolbar';
+import MainLayout from '../../components/MainLayOut/mainLayout.component';
+import DataTable from '../../components/DataTable/databable';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import * as url from '../../helpers/urls';
+import * as fetch from '../../helpers/fetch';
+import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import { UserContext } from '../../Context/userContext';
+import BackdropSpinner from '../../components/BackDrop/backDrop';
 
 const Users = () => {
-  //state
-  const [isLoading, setIsLoading] = useState(false);
-  const [rows, setRows] = useState({});
-  const [dataExport, setDataExport] = useState([]);
-  const [resultados, setResultados] = useState([]);
-  const [searchField, setSearchField] = useState("");
-  const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(50);
-  const [atrib, setAtrib] = useState("name");
-  const [order, setOrder] = useState("asc");
+	//state
+	const [isLoading, setIsLoading] = useState(false);
+	const [rows, setRows] = useState({});
+	const [dataExport, setDataExport] = useState([]);
+	const [resultados, setResultados] = useState([]);
+	const [searchField, setSearchField] = useState('');
+	const [page, setPage] = useState(1);
+	const [limit, setLimit] = useState(50);
+	const [atrib, setAtrib] = useState('name');
+	const [order, setOrder] = useState('asc');
 
-  const handleChangePage = (page) => setPage(page++);
-  const handleChangeLimit = (limit) => setLimit(limit);
-  const handleChangeAtrib = (atrib) => setAtrib(atrib);
-  const handleChangeOrder = (order) => setOrder(order);
-  const history = useHistory();
-  const [user] = useContext(UserContext);
+	const handleChangePage = (page) => setPage(page++);
+	const handleChangeLimit = (limit) => setLimit(limit);
+	const handleChangeAtrib = (atrib) => setAtrib(atrib);
+	const handleChangeOrder = (order) => setOrder(order);
+	const history = useHistory();
+	const [user] = useContext(UserContext);
 
-  const { results } = rows;
-  const { total } = rows;
+	const { results } = rows;
+	const { total } = rows;
 
-  const columns = [
-    { tittle: "Nombres", atributo: "name" },
-    { tittle: "Usuario", atributo: "username" },
-    { tittle: "Rol", atributo: "rol" },
-    { tittle: "Telefono", atributo: "contact_number" },
-    { tittle: "Estado", atributo: "estado" },
-    { tittle: "última actividad", atributo: "last_activity" },
-  ];
+	const columns = [
+		{ tittle: 'Nombres', atributo: 'name' },
+		{ tittle: 'Usuario', atributo: 'username' },
+		{ tittle: 'Rol', atributo: 'rol' },
+		{ tittle: 'Telefono', atributo: 'contact_number' },
+		{ tittle: 'Estado', atributo: 'estado' },
+		{ tittle: 'última actividad', atributo: 'last_activity' },
+	];
 
-  //funciones
+	//funciones
 
-  const getAllusersURL = url.getAllUsersUrl();
-  const getSearchUserURL = url.getSearchUsersUrl();
-  const headerSearch = fetch.requestHeader("GET", null, localStorage.token);
-  const header = fetch.requestHeader("GET", null, localStorage.token);
-  const changeServiceUrl = url.getUserEstadoChangeUrl();
+	const getAllusersURL = url.getAllUsersUrl();
+	const getSearchUserURL = url.getSearchUsersUrl();
+	const headerSearch = fetch.requestHeader('GET', null, localStorage.token);
+	const header = fetch.requestHeader('GET', null, localStorage.token);
+	const changeServiceUrl = url.getUserEstadoChangeUrl();
 
-  const handleOnChangeTextField = (event) => {
-    setSearchField(event.target.value);
-    if (searchField.length >= 3) {
-      const fetchData = async (url, header, setter) => {
-        const loggedInfo = await fetch.fetchData(url, header);
-        fetch.UnauthorizedRedirect(loggedInfo, history);
-        setter(loggedInfo);
-      };
-      fetchData(
-        `${getSearchUserURL}?text=${searchField}`,
-        headerSearch,
-        setResultados,
-      );
-    } else {
-      setResultados([]);
-    }
-  };
+	const handleOnChangeTextField = (event) => {
+		setSearchField(event.target.value);
+		if (searchField.length >= 3) {
+			const fetchData = async (url, header, setter) => {
+				const loggedInfo = await fetch.fetchData(url, header);
+				fetch.UnauthorizedRedirect(loggedInfo, history);
+				setter(loggedInfo);
+			};
+			fetchData(
+				`${getSearchUserURL}?text=${searchField}`,
+				headerSearch,
+				setResultados,
+			);
+		} else {
+			setResultados([]);
+		}
+	};
 
-  const updateEstado = async (id, estado) => {
-    const loggedInfo = await fetch.fetchData(
-      `${changeServiceUrl}?estado=${!estado}&user_id=${id}`,
-      header,
-    );
-    fetch.UnauthorizedRedirect(loggedInfo, history);
-    if (loggedInfo === "changed") {
-      fetchData(
-        `${getAllusersURL}?page=${page}&limit=${limit}&atrib=${atrib}&order=${order}`,
-        header,
-        setRows,
-      );
-    }
-  };
+	const updateEstado = async (id, estado) => {
+		const loggedInfo = await fetch.fetchData(
+			`${changeServiceUrl}?estado=${!estado}&user_id=${id}`,
+			header,
+		);
+		fetch.UnauthorizedRedirect(loggedInfo, history);
+		if (loggedInfo === 'changed') {
+			fetchData(
+				`${getAllusersURL}?page=${page}&limit=${limit}&atrib=${atrib}&order=${order}`,
+				header,
+				setRows,
+			);
+		}
+	};
 
-  const fetchData = async (url, header, setter) => {
-    const loggedInfo = await fetch.fetchData(url, header);
-    fetch.UnauthorizedRedirect(loggedInfo, history);
-    setter(loggedInfo);
-  };
+	const fetchData = async (url, header, setter) => {
+		const loggedInfo = await fetch.fetchData(url, header);
+		fetch.UnauthorizedRedirect(loggedInfo, history);
+		setter(loggedInfo);
+	};
 
-  //efectos
+	//efectos
 
-  useEffect(() => {
-    fetch.UserRedirect(user, history);
-    const header = fetch.requestHeader("GET", null, localStorage.token);
-    const fetchData = async (url, header, setter) => {
-      const loggedInfo = await fetch.fetchData(url, header);
-      fetch.UnauthorizedRedirect(loggedInfo, history);
-      setter(loggedInfo);
-    };
-    setIsLoading(true);
-    fetchData(
-      `${getAllusersURL}?page=${page}&limit=${limit}&atrib=${atrib}&order=${order}`,
-      header,
-      setRows,
-    );
-    fetchData(`${getAllusersURL}?excel=${true}`, header, setDataExport);
-    setIsLoading(false);
-  }, [user, history, page, limit, atrib, order, getAllusersURL]);
+	useEffect(() => {
+		fetch.UserRedirect(user, history);
+		const header = fetch.requestHeader('GET', null, localStorage.token);
+		const fetchData = async (url, header, setter) => {
+			const loggedInfo = await fetch.fetchData(url, header);
+			fetch.UnauthorizedRedirect(loggedInfo, history);
+			setter(loggedInfo);
+		};
+		setIsLoading(true);
+		fetchData(
+			`${getAllusersURL}?page=${page}&limit=${limit}&atrib=${atrib}&order=${order}`,
+			header,
+			setRows,
+		);
+		fetchData(`${getAllusersURL}?excel=${true}`, header, setDataExport);
+		setIsLoading(false);
+	}, [user, history, page, limit, atrib, order, getAllusersURL]);
 
-  return (
-    <MainLayout Tittle="Usuarios">
-      <Container maxWidth={false}>
-        <Toolbar
-          isLoading={isLoading}
-          resultados={resultados}
-          handleOnChangeTextField={handleOnChangeTextField}
-          searchField={searchField}
-          nav="Agregar Usuario"
-          ruta="/create-user"
-          searchLabel="Buscar usuarios"
-          dataExport={dataExport}
-          filename={`usuarios / ${moment().format("MMMM Do YYYY, h:mm")}`}
-        />
-        <Box mt={3}>
-          <DataTable
-            columns={columns}
-            total={total}
-            page={page}
-            limit={limit}
-            atrib={atrib}
-            order={order}
-            handleChangeLimit={handleChangeLimit}
-            handleChangePage={handleChangePage}
-            handleChangeAtrib={handleChangeAtrib}
-            handleChangeOrder={handleChangeOrder}
-          >
-            {results?.length > 0 ? (
-              results.map((row) => (
-                <TableRow key={row.user_id}>
-                  <TableCell align="center">
-                    <Link to={`/edit-user/${row.user_id}`}>
-                      {`${row.name} ${row.lastname}`}
-                    </Link>
-                  </TableCell>
-                  <TableCell align="center">{row.username}</TableCell>
-                  <TableCell align="center">{row.rol}</TableCell>
-                  <TableCell align="center">{row.contact_number}</TableCell>
-                  <TableCell align="center">
-                    <Switch
-                      checked={row.estado === 1 ? true : false}
-                      color="primary"
-                      inputProps={{
-                        "aria-label": "primary checkbox",
-                      }}
-                      onChange={() => {
-                        updateEstado(
-                          row.user_id,
-                          row.estado === 1 ? true : false,
-                        );
-                      }}
-                    />
-                  </TableCell>
-                  <TableCell align="center">
-                    {row.last_activity
-                      ? moment(row.last_activity).fromNow()
-                      : ""}
-                  </TableCell>
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                {results?.length === 0 ? (
-                  columns.map((value) => {
-                    return <TableCell key={value.tittle}></TableCell>;
-                  })
-                ) : (
-                  <BackdropSpinner isLoading={isLoading} />
-                )}
-              </TableRow>
-            )}
-          </DataTable>
-        </Box>
-      </Container>
-    </MainLayout>
-  );
+	return (
+		<MainLayout Tittle="Usuarios">
+			<Container maxWidth={false}>
+				<Toolbar
+					isLoading={isLoading}
+					resultados={resultados}
+					handleOnChangeTextField={handleOnChangeTextField}
+					searchField={searchField}
+					nav="Agregar Usuario"
+					ruta="/create-user"
+					searchLabel="Buscar usuarios"
+					dataExport={dataExport}
+					filename={`usuarios / ${moment().format('MMMM Do YYYY, h:mm')}`}
+				/>
+				<Box mt={3}>
+					<DataTable
+						columns={columns}
+						total={total}
+						page={page}
+						limit={limit}
+						atrib={atrib}
+						order={order}
+						handleChangeLimit={handleChangeLimit}
+						handleChangePage={handleChangePage}
+						handleChangeAtrib={handleChangeAtrib}
+						handleChangeOrder={handleChangeOrder}
+					>
+						{results?.length > 0 ? (
+							results.map((row) => (
+								<TableRow key={row.user_id}>
+									<TableCell align="center">
+										<Link to={`/edit-user/${row.user_id}`}>
+											{`${row.name} ${row.lastname}`}
+										</Link>
+									</TableCell>
+									<TableCell align="center">{row.username}</TableCell>
+									<TableCell align="center">{row.rol}</TableCell>
+									<TableCell align="center">{row.contact_number}</TableCell>
+									<TableCell align="center">
+										<Switch
+											checked={row.estado === 1 ? true : false}
+											color="primary"
+											inputProps={{
+												'aria-label': 'primary checkbox',
+											}}
+											onChange={() => {
+												updateEstado(
+													row.user_id,
+													row.estado === 1 ? true : false,
+												);
+											}}
+										/>
+									</TableCell>
+									<TableCell align="center">
+										{row.last_activity
+											? moment(row.last_activity).fromNow()
+											: ''}
+									</TableCell>
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								{results?.length === 0 ? (
+									columns.map((value) => {
+										return <TableCell key={value.tittle}></TableCell>;
+									})
+								) : (
+									<BackdropSpinner isLoading={isLoading} />
+								)}
+							</TableRow>
+						)}
+					</DataTable>
+				</Box>
+			</Container>
+		</MainLayout>
+	);
 };
 
 export default memo(Users);
