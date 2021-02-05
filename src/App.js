0bc12a52fb5487bf60c8acc7143edf85c0e184dc
Anthony Cardoze <anthonycardoze@gmail.com>
2021-02-05 15:34:30 -0500

    agregando mejoras al sistema de authenticacion

--- src/App.js
+++ src/App.js
@@ -1,51 +1,118 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core';
-import theme from './theme/';
-import { PrivateRoute } from './components/PrivateRoute';
-import Login from './pages/login/';
-import Dashboard from './pages/dashboard/';
-import ProfilePage from './pages/profile/';
-import NotFoundView from './pages/noView/';
-import Users from './pages/users/';
-import UsersEdit from './pages/users/userEdit';
-import UsersCreate from './pages/users/userCreate';
-import Pedidos from './pages/pedidos';
-import Ordenes from './pages/ordenes';
-import NewOrders from './pages/ordenes/newOrders';
-import MisOrdenes from './pages/misOrdenes/';
-import EditOrder from './pages/ordenes/editOrders';
-import { SubcriberRefreshToken } from './helpers/fetch';
-import { UserProvider } from './Context/userContext';
-import { OrderProvider } from './Context/OrderContext';
+import React, { useEffect } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core";
+import theme from "./theme/";
+import { PrivateRoute } from "./components/PrivateRoute";
+import Login from "./pages/login/";
+import Dashboard from "./pages/dashboard/";
+import ProfilePage from "./pages/profile/";
+import NotFoundView from "./pages/noView/";
+import Users from "./pages/users/";
+import UsersEdit from "./pages/users/userEdit";
+import UsersCreate from "./pages/users/userCreate";
+import Pedidos from "./pages/pedidos";
+import Ordenes from "./pages/ordenes";
+import NewOrders from "./pages/ordenes/newOrders";
+import MisOrdenes from "./pages/misOrdenes/";
+import EditOrder from "./pages/ordenes/editOrders";
+import { UserProvider } from "./Context/userContext";
+import { OrderProvider } from "./Context/OrderContext";
+import { useJwt } from "react-jwt";
+import * as url from "./helpers/urls";
+import * as fetch from "./helpers/fetch";
 
 function App() {
-	return (
-		<SubcriberRefreshToken>
-			<UserProvider>
-				<OrderProvider>
-					<ThemeProvider theme={theme}>
-						<CssBaseline />
-						<Switch>
-							<PrivateRoute exact path="/" component={Dashboard} />
-							<PrivateRoute path="/profile" component={ProfilePage} />
-							<PrivateRoute path="/users" component={Users} />
-							<PrivateRoute path="/pedidos" component={Pedidos} />
-							<PrivateRoute path="/orders" component={Ordenes} />
-							<PrivateRoute path="/edit-orders/:id" component={EditOrder} />
-							<PrivateRoute path="/create-orders" component={NewOrders} />
-							<PrivateRoute path="/my-orders" component={MisOrdenes} />
-							<PrivateRoute path="/create-user" component={UsersCreate} />
-							<PrivateRoute path="/edit-user/:id" component={UsersEdit} />
-							<Route path="/login" component={Login} />
-							<Route path="*" component={NotFoundView} />
-						</Switch>
-					</ThemeProvider>
-				</OrderProvider>
-			</UserProvider>
-		</SubcriberRefreshToken>
-	);
+  const history = useHistory();
+  const { isExpired } = useJwt(localStorage.accessToken);
+
+  useEffect(() => {
+    console.log("background job...");
+    const interval = setInterval(async () => {
+      console.log("running...");
+      if (localStorage.token && localStorage.refresh_token) {
+        console.log("both token exist...");
+        if (!isExpired) return;
+
+        console.log("token is not expired...");
+        const body = JSON.stringify({
+          token: localStorage.refresh_token,
+        });
+        const TokenRenewServiceUrl = url.refreshTokenUrl();
+        const headerRT = fetch.requestHeader("POST", body, "");
+
+        try {
+          const loggedInfo = await fetch.fetchData(
+            TokenRenewServiceUrl,
+            headerRT,
+          );
+          console.log(loggedInfo);
+          if (loggedInfo.accessToken) {
+            localStorage.setItem("token", loggedInfo.accessToken);
+            const getUserData = url.getUserUrl();
+            const headerGetData = fetch.requestHeader(
+              "GET",
+              null,
+              loggedInfo.accessToken,
+            );
+            const userdata = await fetch.fetchData(getUserData, headerGetData);
+            console.log(userdata);
+            if (userdata) {
+              localStorage.removeItem("user");
+              localStorage.setItem(
+                "user",
+                JSON.stringify({
+                  user_id: userdata[0].user_id,
+                  name: userdata[0].name,
+                  lastname: userdata[0].lastname,
+                  email: userdata[0].correo_electronico,
+                  number: userdata[0].contact_number,
+                  id_pais: userdata[0].id_pais,
+                  address: userdata[0].address,
+                  pais: userdata[0].pais,
+                  estado: userdata[0].estado,
+                  rol: userdata[0].rol,
+                  username: userdata[0].username,
+                }),
+              );
+            } else {
+              return history.push("/login");
+            }
+          } else {
+            return history.push("/login");
+          }
+        } catch (error) {
+          return history.push("/login");
+        }
+      }
+    }, 900000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+  return (
+    <UserProvider>
+      <OrderProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <PrivateRoute path="/profile" component={ProfilePage} />
+            <PrivateRoute path="/users" component={Users} />
+            <PrivateRoute path="/pedidos" component={Pedidos} />
+            <PrivateRoute path="/orders" component={Ordenes} />
+            <PrivateRoute path="/edit-orders/:id" component={EditOrder} />
+            <PrivateRoute path="/create-orders" component={NewOrders} />
+            <PrivateRoute path="/my-orders" component={MisOrdenes} />
+            <PrivateRoute path="/create-user" component={UsersCreate} />
+            <PrivateRoute path="/edit-user/:id" component={UsersEdit} />
+            <Route path="/login" component={Login} />
+            <Route path="*" component={NotFoundView} />
+          </Switch>
+        </ThemeProvider>
+      </OrderProvider>
+    </UserProvider>
+  );
 }
 
 export default App;
--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,114 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import * as url from './urls';
-import { useHistory } from 'react-router-dom';
-import { useJwt } from 'react-jwt';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import * as url from "./urls";
+import { useHistory } from "react-router-dom";
+import { useJwt } from "react-jwt";
 
 export const requestHeader = (method, body = {}, token) => {
-	return {
-		method,
-		headers: {
-			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*',
-			Authorization: token ? `Bearer ${token}` : '',
-		},
-		mode: 'cors',
-		cache: 'default',
-		body: body,
-	};
+  return {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+    mode: "cors",
+    cache: "default",
+    body: body,
+  };
 };
 
 requestHeader.PropTypes = {
-	method: PropTypes.string,
-	token: PropTypes.string,
-	body: PropTypes.object,
+  method: PropTypes.string,
+  token: PropTypes.string,
+  body: PropTypes.object,
 };
 
 export const fetchData = async (urls, header) => {
-	try {
-		const query = await fetch(urls, header);
-		const parsed = await query.json();
-		return parsed;
+  try {
+    const query = await fetch(urls, header);
+    const parsed = await query.json();
+    return parsed;
 
-		// if (parsed === 'No esta autorizado') {
-		// 	const body = JSON.stringify({
-		// 		token: localStorage.refresh_token ? localStorage.refresh_token : null,
-		// 	});
-		// 	const TokenRenewServiceUrl = url.refreshTokenUrl();
-		// 	const headerRT = requestHeader('POST', body, '');
-		// 	const loggedInfo = await fetchData(TokenRenewServiceUrl, headerRT);
-		// 	if (loggedInfo.accessToken) {
-		// 		localStorage.setItem('token', loggedInfo.accessToken);
-		// 		header.headers.Authorization = `Bearer ${loggedInfo.accessToken}`;
-		// 		const requery = await fetch(urls, header);
-		// 		const reparsed = await requery.json();
-		// 		console.log('reparsed:', parsed);
-		// 		return reparsed;
-		// 	} else {
-		// 		console.log('No esta autorizado');
-		// 		return 'No esta autorizado';
-		// 	}
-		// } else {
-		// 	console.log('parsed:', parsed);
-		// 	return parsed;
-		// }
-	} catch (error) {
-		console.log('error: ', error);
-		return 'conexion error';
-	}
+    // if (parsed === 'No esta autorizado') {
+    // 	const body = JSON.stringify({
+    // 		token: localStorage.refresh_token ? localStorage.refresh_token : null,
+    // 	});
+    // 	const TokenRenewServiceUrl = url.refreshTokenUrl();
+    // 	const headerRT = requestHeader('POST', body, '');
+    // 	const loggedInfo = await fetchData(TokenRenewServiceUrl, headerRT);
+    // 	if (loggedInfo.accessToken) {
+    // 		localStorage.setItem('token', loggedInfo.accessToken);
+    // 		header.headers.Authorization = `Bearer ${loggedInfo.accessToken}`;
+    // 		const requery = await fetch(urls, header);
+    // 		const reparsed = await requery.json();
+    // 		console.log('reparsed:', parsed);
+    // 		return reparsed;
+    // 	} else {
+    // 		console.log('No esta autorizado');
+    // 		return 'No esta autorizado';
+    // 	}
+    // } else {
+    // 	console.log('parsed:', parsed);
+    // 	return parsed;
+    // }
+  } catch (error) {
+    console.log("error: ", error);
+    return "conexion error";
+  }
 };
 
 export const SubcriberRefreshToken = ({ children }) => {
-	const history = useHistory();
-	const { isExpired } = useJwt(localStorage.accessToken);
-	useEffect(() => {
-		const interval = setInterval(async () => {
-			if (isExpired && localStorage.refresh_token) {
-				const body = JSON.stringify({
-					token: localStorage.refresh_token,
-				});
-				const TokenRenewServiceUrl = url.refreshTokenUrl();
-				const headerRT = requestHeader('POST', body, '');
-				const loggedInfo = await fetchData(TokenRenewServiceUrl, headerRT);
-				if (loggedInfo.accessToken) {
-					localStorage.setItem('token', loggedInfo.accessToken);
-				} else {
-					return history.push('/login');
-				}
-			}
-		}, 180000);
+  const history = useHistory();
+  const { isExpired } = useJwt(localStorage.accessToken);
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      if (isExpired && localStorage.refresh_token) {
+        const body = JSON.stringify({
+          token: localStorage.refresh_token,
+        });
+        const TokenRenewServiceUrl = url.refreshTokenUrl();
+        const headerRT = requestHeader("POST", body, "");
+        const loggedInfo = await fetchData(TokenRenewServiceUrl, headerRT);
+        if (loggedInfo.accessToken) {
+          localStorage.setItem("token", loggedInfo.accessToken);
+        } else {
+          return history.push("/login");
+        }
+      }
+    }, 180000);
 
-		return () => {
-			clearInterval(interval);
-		};
-	});
-	return <>{children}</>;
+    return () => {
+      clearInterval(interval);
+    };
+  });
+  return <>{children}</>;
 };
 
 export function useStickyState(defaultValue, key) {
-	const [value, setValue] = useState(() => {
-		const stickyValue = localStorage.getItem(key);
-		return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
-	});
-	useEffect(() => {
-		localStorage.setItem(key, JSON.stringify(value));
-	}, [key, value]);
-	return [value, setValue];
+  const [value, setValue] = useState(() => {
+    const stickyValue = localStorage.getItem(key);
+    return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
+  });
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
 }
 
 export const UnauthorizedRedirect = (data, history) => {
-	if (data === 'conexion error' || data === 'No esta autorizado') {
-		localStorage.removeItem('token');
-		localStorage.removeItem('refresh_token');
-		history.push('/login');
-	}
+  if (data === "conexion error" || data === "No esta autorizado") {
+    localStorage.clear();
+    history.push("/login");
+  }
 };
 
 export const UserRedirect = (user, history) => {
-	if (user && user.rol !== 'Administrador')
-		return history.push('/create-orders');
+  if (user && user.rol !== "Administrador")
+    return history.push("/create-orders");
 };
 
 export const numberWithCommas = (x) => {
-	return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 };
